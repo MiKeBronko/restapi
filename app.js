5a73a0ced1a986fe910491e450decfd1fd9588fb
Mikhail Fatkin <mfo787@gmail.com>
2019-11-25 16:16:11 +0300

    add 2 routes by user

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const app = express();
 const mongoose = require('mongoose');
-const path = require('path');
+// const path = require('path');
 const bodyParser = require('body-parser');
 const users = require('./routes/users');
 const cards = require('./routes/cards');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,21 +18,19 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-app.use('/', users);
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '5dd948e1cef68f1fecdbbf32',
+    _id: '5dd3e0673dd2fc0a70b25c5e',
   };
+  console.log(req.user._id);
   next();
 });
 
+app.use('/', users);
 app.use('/', cards);
 
-
 const { PORT = 3000 } = process.env;
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/*' || '*', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
